8ef9c2ebfe1e14cbf80ac5e18fed16a741c5a18b
Farfadeli <ndaunac@gmail.com>
2025-10-08 17:24:05 +0200

    update requirements

--- launch.ps1
+++ launch.ps1
@@ -1,4 +1,9 @@
 param ($population, $gap, $limit, $file)
 
+
+# create python venv if not exist and activate it
+python -m venv world_sims
 ./world_sims/Scripts/activate
+
+
 python ./world_simulation.py --population $population --gap $gap --limit $limit --file $file
